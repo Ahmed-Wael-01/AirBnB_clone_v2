1c41a4880c7aa2e238abb4e4dc4f6e64c00b9837
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-08-02 09:08:05 +0300

    extra

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-"""do packing"""
-from fabric.api import *
-from os.path import exists
+"""Generates a .tgz archive and distributes the archive to the web servers"""
+
+from fabric.api import run, env, put
+from os import path
+
 env.hosts = ['54.209.62.255', '52.23.179.43']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """packing stuff"""
-    if exists(archive_path) is False:
+    """Distributes the .tgz archive to the web servers"""
+
+    if not path.exists(archive_path):
         return False
+
     try:
-        zfile = archive_path.split('/')[-1]
-        unexe = zfile.split('.')[0]
-        put(archive_path, '/tmp/')
-        sudo('mkdir -p {}{}/'.format('/data/web_static/releases/', unexe))
-        sudo('tar -xzf /tmp/{} -C {}{}/'.format(
-                    zfile, '/data/web_static/releases/', unexe))
-        sudo('rm /tmp/{}'.format(zfile))
-        sudo('rsync -a {0}{1}/web_static/ {0}{1}/'.format(
-            '/data/web_static/releases/', unexe))
-        sudo('rm -rf {0}{1}/web_static/*'.format(
-            '/data/web_static/releases/', unexe))
-        sudo('rm -rf {}{}/web_static'.format(
-            '/data/web_static/releases/', unexe))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}{}/ /data/web_static/current'.format(
-            '/data/web_static/releases/', unexe))
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/{}".format(
+            archived_file[:-4])
+        archived_file = "/tmp/{}".format(archived_file)
+
+        put(archive_path, "/tmp/")
+        sudo("sudo mkdir -p {}".format(newest_version))
+        sudo("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        sudo("sudo rm {}".format(archived_file))
+        sudo("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        sudo("sudo rm -rf {}/web_static".format(newest_version))
+        sudo("sudo rm -rf /data/web_static/current")
+        sudo("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
+
         return True
-    except Exception as e:
+    except Exception:
         return False
