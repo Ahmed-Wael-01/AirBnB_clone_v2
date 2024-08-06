2fced87e3bd03129a7d49a90b9f7548927b89b26
Ahmed Wael <xahmedwaheed@gmail.com>
2024-08-06 06:11:10 -0700

    four

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""basic flask app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """handle root route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """handle root route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def textes(text):
+    """handle root route"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonista(text="is cool"):
+    """handle root route"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numbers(n):
+    """handle root route"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
