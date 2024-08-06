5040d5300a5cc94aa7d979979e0bd556afa17913
Ahmed Wael <xahmedwaheed@gmail.com>
2024-08-06 16:50:21 -0700

    eight

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""basic flask app"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tearit():
+    """closes session after each request"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_listing():
+    """display a page with states"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
