0669a7ae0d867e1965c00d83748fcd0910bee8c8
Ahmed Wael <xahmedwaheed@gmail.com>
2024-08-06 18:20:19 -0700

    fixes eight

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""basic flask app"""
+"""start Flask application"""
+
 from flask import Flask, render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def tearit(e):
-    """closes session after each request"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
-def states_listing():
-    """display a page with states"""
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 
 if __name__ == '__main__':
